da9a958e46b7bd7debc56051c90648e2336f54ae
almartin82 <almartin@gmail.com>
2026-01-07 10:50:22 -0500

    Remove non-standard files and add fetch_graduation to Python wrapper

--- pyndschooldata/pyndschooldata/__init__.py
+++ pyndschooldata/pyndschooldata/__init__.py
@@ -10,7 +10,8 @@ from .core import (
     fetch_enr_multi,
     tidy_enr,
     get_available_years,
+    fetch_graduation,
 )
 
 __version__ = "0.1.0"
-__all__ = ["fetch_enr", "fetch_enr_multi", "tidy_enr", "get_available_years"]
+__all__ = ["fetch_enr", "fetch_enr_multi", "tidy_enr", "get_available_years", "fetch_graduation"]
